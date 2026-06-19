8ed5b7e49f98fa77165ba56a3cfdbc2105a30f12
NormalV <moritz@fackler.cloud>
2026-06-19 10:23:53 +0200

    fix: baritone pathing flickering

--- src/main/java/net/normalv/systems/fightbot/pathing/PathingHelper.java
+++ src/main/java/net/normalv/systems/fightbot/pathing/PathingHelper.java
@@ -3,25 +3,35 @@ package net.normalv.systems.fightbot.pathing;
 import baritone.api.BaritoneAPI;
 import net.minecraft.world.entity.Entity;
 import net.normalv.BlockFighter;
+import net.normalv.event.events.impl.TargetChangeEvent;
+import net.normalv.event.system.Subscribe;
 import net.normalv.util.Util;
 
 public class PathingHelper implements Util {
     private double baritoneUseDistance = 20;
     private boolean onlyUseBaritone = false;
-    private boolean pathing = false;
+    private boolean useBaritone;
     private boolean allowJumping = true;
 
-    public void goToEntity(Entity target) {
-        pathing = true;
+    public PathingHelper() {
+        EVENT_BUS.register(this);
+    }
 
-        if(shouldUseBaritone(target)) {
+    public void goToEntity(Entity target) {
+        updateUseBaritone(target);
+
+        if(useBaritone && !BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().isPathing()) {
+            releaseAllKeys();
             BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new FollowEntityGoal(target, BlockFighter.fightBot.getMaxReach()-0.1));
             return;
         }
-        else if(BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().isPathing()) stopPathing();
 
-        float[] rotation = BlockFighter.playerManager.calcAngle(mc.player.getEyePosition(), target.getEyePosition());
-        manualPathing(rotation, target);
+        if(!useBaritone) {
+            if(BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().isPathing()) BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().cancelEverything();
+
+            float[] rotation = BlockFighter.playerManager.calcAngle(mc.player.getEyePosition(), target.getEyePosition());
+            manualPathing(rotation, target);
+        }
     }
 
     public void manualPathing(float[] rotation, Entity target) {
@@ -43,18 +53,26 @@ public class PathingHelper implements Util {
         }
     }
 
-    public boolean shouldUseBaritone(Entity target) {
-        if(onlyUseBaritone) return true;
-        return mc.player.distanceTo(target)>=baritoneUseDistance;
+    public void updateUseBaritone(Entity target) {
+        useBaritone = mc.player.distanceTo(target)>=baritoneUseDistance || !mc.player.hasLineOfSight(target) || onlyUseBaritone;
     }
 
     public void stopPathing() {
         BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().cancelEverything();
-        pathing = false;
     }
 
-    public boolean isPathing() {
-        return pathing;
+    @Subscribe
+    public void onTargetChange(TargetChangeEvent event) {
+        stopPathing();
+        releaseAllKeys();
+    }
+
+    public void releaseAllKeys() {
+        mc.options.keyDown.setDown(false);
+        mc.options.keyUp.setDown(false);
+        mc.options.keyJump.setDown(false);
+        mc.options.keyLeft.setDown(false);
+        mc.options.keyRight.setDown(false);
     }
 
     public boolean onlyUseBaritone() {
